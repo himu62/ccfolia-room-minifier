81a9fedef25faa9616a2de8261bdc44eb5d36bb5
Hiromu Kobayashi (himu) <himu62@users.noreply.github.com>
2024-12-24 18:27:44 +0900

    Revert "relative path"

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { fileSystemTreePlugin } from './script/generate-webcontainer-fstree'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [
     react(),
     fileSystemTreePlugin("./webcontainer"),
